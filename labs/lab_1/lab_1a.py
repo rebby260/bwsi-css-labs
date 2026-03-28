44d0b132351cf68cd480b385e8181b31ddd1e9bb
rebby260 <reblemrus@gmail.com>
2026-03-28 15:41:01 -0400

    Add name & Introduction lab_1a.py

--- labs/lab_1/lab_1a.py
+++ labs/lab_1/lab_1a.py
@@ -11,6 +11,7 @@ def main():
     name = "Rebecca Lemiesa" # TODO: Insert your name between the double quotes
 
     print(f"{name}, Welcome to the CSS course!")
+    print("Pretty late on this but what's up guys! I'm Rebecca, a sophmore from Maryland. Anything connected to biology and computer science, particularly java, interests me and I like playing violin and tennis when I realize I need to go outside... As of now, the online courses I'm still completing truly are revealing more about the whole world about computer science which I have now come to realize is much more confusing than I thought! Learning a lot so far though so no complaints here! See you guys later.")
 
 if __name__ == "__main__":
     main()
